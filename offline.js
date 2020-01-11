87b54c7473de97b17c4671d90bda5619a9b3daad
guruchian <1210@sts.ac.th>
2020-01-11 22:03:19 +0700

    wordgamex

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1578754942,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/stroke-sheet0.png",
+		"images/marker-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
